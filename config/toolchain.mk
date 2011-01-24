e9b4b3d26eae1a07c9c2d948e51250792beea075
Pixel <pixel@nobis-crew.org>
2011-01-23 18:00:29 -0800

    Tweaking a few more flags, and adding more symbols to the ldscript.

--- config/toolchain.mk
+++ config/toolchain.mk
@@ -3,7 +3,7 @@ TOOLCHAIN = arm-none-eabi
 ifeq ($(CPU_FLAVOR),lpc1768)
 TARGET_CPPFLAGS += -mcpu=cortex-m3 -mtune=cortex-m3 -D__thumb2__=1 -march=armv7-m -mfix-cortex-m3-ldrd
 endif
-TARGET_CPPFLAGS += -mthumb -Os -mapcs-frame -msoft-float -mno-sched-prolog -fno-hosted -ffunction-sections -fdata-sections
+TARGET_CPPFLAGS += -mthumb -Os -mapcs-frame -msoft-float -mno-sched-prolog -fno-hosted -ffunction-sections -fdata-sections -fshort-wchar
 endif
 
 TARGET_CC = $(TOOLCHAIN)-gcc
