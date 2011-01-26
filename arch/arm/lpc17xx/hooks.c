30306a42972f6a414e8b23e7942fd77f74af80db
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2011-01-27 00:04:27 +0100

    Adding the osdebug mechanism into arch and FreeRTOS, essentially closing the circle of dependancy between all 3 libraries. Also, turning the debugging on by default.

--- arch/arm/lpc17xx/hooks.c
+++ arch/arm/lpc17xx/hooks.c
@@ -1,7 +1,19 @@
 #include "FreeRTOS.h"
 #include "task.h"
+#include "osdebug.h"
 
-void vConfigureTimerForRunTimeStats() { }
-void vApplicationTickHook() { }
-void vApplicationStackOverflowHook( xTaskHandle *pxTask, signed portCHAR *pcTaskName ) { }
-void vApplicationIdleHook() { }
+void vConfigureTimerForRunTimeStats() {
+    DBGOUT("vConfigureTimerForRunTimeStats()\r\n");
+}
+
+void vApplicationTickHook() {
+    DBGOUT("vApplicationTickHook()\r\n");
+}
+
+void vApplicationStackOverflowHook(xTaskHandle *pxTask, signed portCHAR *pcTaskName) {
+    DBGOUT("vApplicationStackOverflowHook(%p, %s)\r\n", pxTask, pcTaskName);
+}
+
+void vApplicationIdleHook() {
+    DBGOUT("vApplicationIdleHook()\r\n");
+}
--- config/general.mk
+++ config/general.mk
@@ -1,5 +1,10 @@
 VERBOSE = false
+DEBUG = true
 
 ifneq ($(VERBOSE),true)
 MAKE_OPTS = --no-print-directory -s
+endif
+
+ifeq ($(DEBUG),true)
+TARGET_CPPFLAGS += -DFULLDEBUG
 endif
