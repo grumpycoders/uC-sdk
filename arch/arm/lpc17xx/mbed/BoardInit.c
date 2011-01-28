6c62aa73a40ab548d62ca804290a5c305f539fa9
Pixel <pixel@nobis-crew.org>
2011-01-27 22:34:58 -0800

    Powering down the CPU in case of an exception.

--- arch/arm/lpc17xx/mbed/BoardInit.c
+++ arch/arm/lpc17xx/mbed/BoardInit.c
@@ -1,4 +1,5 @@
 #include "lpc17xx_nvic.h"
+#include "lpc17xx_clkpwr.h"
 
 extern uintptr_t __cs3_interrupt_vector_mutable[];
 
@@ -13,4 +14,7 @@ void BoardShutdown() {
 }
 
 void BoardExceptionHandler(int code) {
+    volatile int i;
+    for (i = 0; i < 100000; i++);
+    CLKPWR_DeepPowerDown();
 }
