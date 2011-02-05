4830af498e2d3a5440e65c63a3595b91f1cd4ac9
Pixel <pixel@nobis-crew.org>
2011-02-05 09:40:03 -0800

    Added qsort, stolen from libnix.

--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -9,4 +9,6 @@ typedef void (*atexit_func_t)(void);
 void exit(int status) __attribute__((noreturn));
 int atexit(atexit_func_t);
 
+void qsort(void *base, size_t nel, size_t width, int (*compar)(const void *, const void *));
+
 #endif
