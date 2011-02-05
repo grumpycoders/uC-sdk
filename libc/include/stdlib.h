afe2234fb10053af0401050d81a4ee985a08504f
Pixel <pixel@nobis-crew.org>
2011-02-05 13:15:32 -0800

    Adding a few more stdlib functions.

--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -3,6 +3,7 @@
 
 #include <reent.h>
 #include <malloc.h>
+#include <stdio.h>
 
 typedef void (*atexit_func_t)(void);
 
@@ -10,5 +11,9 @@ void exit(int status) __attribute__((noreturn));
 int atexit(atexit_func_t);
 
 void qsort(void *base, size_t nel, size_t width, int (*compar)(const void *, const void *));
+
+static inline double atof(const char * str) { double r = 0; sscanf(str, "%lf", &r); return r; }
+static inline int atoi(const char * str) { int i; sscanf(str, "%i", &i); return i; }
+static inline int atol(const char * str) { long l; sscanf(str, "%li", &l); return l; }
 
 #endif
