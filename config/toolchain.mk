69b7ba37681df1b981e5a50263497e5d8cd48c5c
Thomas Pietrzak <thomas.pietrzak@gmail.com>
2013-10-09 23:22:57 +0200

    fix awful bug

--- config/toolchain.mk
+++ config/toolchain.mk
@@ -5,7 +5,7 @@ ifeq ($(CPU_FLAVOR),lpc1768)
 TARGET_CPPFLAGS += -mcpu=cortex-m3 -mtune=cortex-m3 -D__thumb2__=1 -march=armv7-m -mfix-cortex-m3-ldrd
 endif
 TARGET_CPPFLAGS += -Os -mapcs-frame -msoft-float -mno-sched-prolog -fno-hosted -ffunction-sections -fdata-sections -fshort-wchar -D__LITTLE_ENDIAN
-TARGET_LDFLAGS += --no-wchar-size-warning
+TARGET_LDFLAGS += -Wl,--no-wchar-size-warning
 TARGET_CFLAGS += -mthumb
 endif
 
