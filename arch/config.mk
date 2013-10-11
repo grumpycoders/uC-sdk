5580e59d57b918c8be0f222ac3782921d47a6b62
Nicolas Noble <nnoble@blizzard.com>
2013-10-11 13:15:13 -0700

    Reorganized the directory tree a bit; CM3 is for more than one kind of CPU, so let's move its shared code.

--- arch/config.mk
+++ arch/config.mk
@@ -1,10 +1,15 @@
 ifeq ($(CPU),arm)
 TARGET_INCLUDES += $(ROOTDIR)/arch/arm/include
 ifeq ($(CPU_FLAVOR),lpc1768)
-TARGET_INCLUDES += $(ROOTDIR)/arch/arm/lpc17xx/Core/CM3/DeviceSupport/NXP/LPC17xx $(ROOTDIR)/arch/arm/lpc17xx/Core/CM3/CoreSupport $(ROOTDIR)/arch/arm/lpc17xx/Drivers/include $(ROOTDIR)/arch/arm/lpc17xx/mbed
+TARGET_INCLUDES += $(ROOTDIR)/arch/arm/lpc17xx/Core/CM3/DeviceSupport/NXP/LPC17xx $(ROOTDIR)/arch/arm/lpc17xx/Drivers/include $(ROOTDIR)/arch/arm/lpc17xx/mbed
 LDSCRIPT = $(ROOTDIR)/arch/arm/lpc17xx/ldscript
 SPECS = $(ROOTDIR)/arch/arm/lpc17xx/specs
 TARGET_CPPFLAGS += -DTARGET_LITTLE_ENDIAN
+CPU_FAMILY = CM3
+endif
+
+ifeq ($(CPU_FAMILY),CM3)
+TARGET_INCLUDES += $(ROOTDIR)/arch/arm/src/Core/CM3/CoreSupport
 endif
 endif
 
