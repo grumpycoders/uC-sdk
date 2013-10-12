c624b9ec11d7b1bed8fd00c852e556f602158bdb
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2013-10-11 23:43:52 -0700

    Adding basic documentation for now, as well as a simple skeleton application.

---
+++ skel/test.c
@@ -0,0 +1,34 @@
+#include <FreeRTOS.h>
+#include <task.h>
+#include <semphr.h>
+#include <BoardConsole.h>
+#include <stdio.h>
+#include <fio.h>
+#include <romfs.h>
+#include <semifs.h>
+#include <malloc_wrapper.h>
+
+static const char msg[] = "Hello world - from fwrite!\r\n";
+
+extern uint8_t _binary_romfs_bin_start[];
+
+int main() {
+    init_malloc_wrapper();
+    FILE * f1, * f2;
+    char buf[32];
+    int c;
+    register_devfs();
+    register_romfs("romfs", _binary_romfs_bin_start);
+    printf("Hello world - from stdio!\r\n");
+    fflush(stdout);
+    f1 = fopen("/dev/stdout", "w");
+    fprintf(stderr, "f1 = %p\r\n", f1);
+    fwrite(msg, 1, sizeof(msg), f1);
+    f2 = fopen("/romfs/test.txt", "r");
+    c = fread(buf, 1, 32, f2);
+    fwrite(buf, 1, c, f1);
+    fflush(f1);
+    fclose(f1);
+    fclose(f2);
+    return 0;
+}
