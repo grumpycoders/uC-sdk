d5aa0b4e43f61618951194f19361097e3197e337
Thomas Pietrzak <thomas.pietrzak@gmail.com>
2014-02-11 15:00:59 +0100

    custom ldscript for stm32f40x

--- arch/config.mk
+++ arch/config.mk
@@ -15,7 +15,6 @@ endif
 
 ifeq ($(CPU_FLAVOR),stm32f4)
 TARGET_INCLUDES += $(ROOTDIR)/arch/arm/stm32f4xx/Core/CM4F/DeviceSupport/ST/STM32F4xx $(ROOTDIR)/arch/arm/stm32f4xx/Drivers/include
-LDSCRIPT = $(ROOTDIR)/arch/arm/stm32f4xx/ldscript
 TARGET_CPPFLAGS += -DTARGET_LITTLE_ENDIAN
 endif
 
@@ -41,6 +40,7 @@ endif
 ifeq ($(BOARD),stm32f4discovery)
 TARGET_INCLUDES += $(ROOTDIR)/arch/arm/stm32f4xx/stm32f4discovery
 TARGET_CPPFLAGS += -DHSE_VALUE=8000000
+LDSCRIPT = $(ROOTDIR)/arch/arm/stm32f4xx/stm32f40x.ld
 endif
 
 endif
