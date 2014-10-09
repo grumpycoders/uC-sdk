701250a54300e5aa68ae03c4bdb035394d1a13e2
Thomas Pietrzak <thomas.pietrzak@gmail.com>
2014-10-09 17:39:21 +0200

    setup clock in earlyinit

--- arch/arm/stm32f4xx/stm32f4discovery/BoardInit.c
+++ arch/arm/stm32f4xx/stm32f4discovery/BoardInit.c
@@ -1,13 +1,13 @@
 #include "stm32f4xx_rcc.h"
 
 void BoardEarlyInit() {
-}
-
-void BoardInit() {
 	/* Select HSE as system clock source */
     RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_SW));
     RCC->CFGR |= (uint32_t)RCC_CFGR_SW_HSE;
     SystemCoreClockUpdate();
+}
+
+void BoardInit() {
 }
 
 void BoardLateInit() {
