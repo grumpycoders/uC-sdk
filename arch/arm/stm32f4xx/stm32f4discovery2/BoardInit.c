d171cefc736e3dc5170e16c36d496fc28b9ae8b8
Thomas Pietrzak <thomas.pietrzak@gmail.com>
2015-03-02 10:53:33 +0100

    remove comments

--- arch/arm/stm32f4xx/stm32f4discovery2/BoardInit.c
+++ arch/arm/stm32f4xx/stm32f4discovery2/BoardInit.c
@@ -4,7 +4,7 @@ void BoardEarlyInit() {
 }
 
 void BoardInit() {
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+    //RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
 }
 
 void BoardLateInit() {
