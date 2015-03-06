ec7ec265c9050ed358fab24cf136e3e35218a8df
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2015-03-06 10:23:45 +0100

    Adding a few more semihosting calls.

--- arch/arm/include/angel.h
+++ arch/arm/include/angel.h
@@ -6,9 +6,14 @@ BEGIN_DECL
 
 int Semihost_SYS_OPEN(uint32_t * args);
 int Semihost_SYS_CLOSE(uint32_t * args);
+int Semihost_SYS_WRITEC(uint32_t * args);
 int Semihost_SYS_WRITE(uint32_t * args);
+int Semihost_SYS_WRITE0(uint32_t * args);
 int Semihost_SYS_READ(uint32_t * args);
+int Semihost_SYS_READC(uint32_t * args);
 int Semihost_SYS_SEEK(uint32_t * args);
 int Semihost_SYS_FLEN(uint32_t * args);
+int Semihost_SYS_REMOVE(uint32_t * args);
+int Semihost_SYS_RENAME(uint32_t * args);
 
 END_DECL
--- arch/arm/src/CM4F/angel.s
+++ arch/arm/src/CM4F/angel.s
@@ -22,6 +22,26 @@ Semihost_SYS_CLOSE:
     bkpt 0xAB
     bx lr
 
+.section ".text.Semihost_SYS_WRITEC", "x", %progbits
+.thumb_func
+.globl Semihost_SYS_WRITEC
+.type Semihost_SYS_WRITEC, %function
+Semihost_SYS_WRITEC:
+    mov r1, r0
+    mov r0, #3
+    bkpt 0xAB
+    bx lr
+
+.section ".text.Semihost_SYS_WRITE0", "x", %progbits
+.thumb_func
+.globl Semihost_SYS_WRITE0
+.type Semihost_SYS_WRITE0, %function
+Semihost_SYS_WRITE0:
+    mov r1, r0
+    mov r0, #4
+    bkpt 0xAB
+    bx lr
+
 .section ".text.Semihost_SYS_WRITE", "x", %progbits
 .thumb_func
 .globl Semihost_SYS_WRITE
@@ -42,6 +62,16 @@ Semihost_SYS_READ:
     bkpt 0xAB
     bx lr
 
+.section ".text.Semihost_SYS_READC", "x", %progbits
+.thumb_func
+.globl Semihost_SYS_READC
+.type Semihost_SYS_READC, %function
+Semihost_SYS_READC:
+    mov r1, r0
+    mov r0, #7
+    bkpt 0xAB
+    bx lr
+
 .section ".text.Semihost_SYS_SEEK", "x", %progbits
 .thumb_func
 .globl Semihost_SYS_SEEK
@@ -59,6 +89,26 @@ Semihost_SYS_SEEK:
 Semihost_SYS_FLEN:
     mov r1, r0
     mov r0, #12
+    bkpt 0xAB
+    bx lr
+
+.section ".text.Semihost_SYS_REMOVE", "x", %progbits
+.thumb_func
+.globl Semihost_SYS_REMOVE
+.type Semihost_SYS_REMOVE, %function
+Semihost_SYS_REMOVE:
+    mov r1, r0
+    mov r0, #14
+    bkpt 0xAB
+    bx lr
+
+.section ".text.Semihost_SYS_RENAME", "x", %progbits
+.thumb_func
+.globl Semihost_SYS_RENAME
+.type Semihost_SYS_RENAME, %function
+Semihost_SYS_RENAME:
+    mov r1, r0
+    mov r0, #15
     bkpt 0xAB
     bx lr
 
