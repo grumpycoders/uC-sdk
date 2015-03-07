592b6e7a461d8be1d2953852effa3b9e4caf1c5a
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2015-03-07 09:00:42 +0100

    Trying to make semiio a bit faster - but slightly less correct.

--- os/src/semiio.c
+++ os/src/semiio.c
@@ -15,11 +15,18 @@ static ssize_t semi_stdin_read(void * opaque, void * buf, size_t count) {
 static ssize_t semi_stdout_write(void * opaque, const void * buf, size_t count) {
     const uint8_t * buffer = buf;
     size_t ret = count;
+#ifdef SEMIIO_CORRECT
     while (count--) {
         if (Semihost_SYS_WRITEC((uint32_t *) buffer++) != 0) {
             return -1;
         }
     }
+#else
+    uint8_t * str = alloca(count + 1);
+    memcpy(str, buf, count);
+    str[count] = 0;
+    Semihost_SYS_WRITE0((uint32_t *) str);
+#endif
 
     return ret;
 }
