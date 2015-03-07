e538e87d7153a5986950cb14da22731dc9ec9e54
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2015-03-07 09:15:35 +0100

    Optimizing further - and even less correctly.

--- os/src/semiio.c
+++ os/src/semiio.c
@@ -14,18 +14,24 @@ static ssize_t semi_stdin_read(void * opaque, void * buf, size_t count) {
 }
 
 static ssize_t semi_stdout_write(void * opaque, const void * buf, size_t count) {
+    const uint8_t * buffer = buf;
     size_t ret = count;
 #ifdef SEMIIO_CORRECT
-    const uint8_t * buffer = buf;
     while (count--) {
         if (Semihost_SYS_WRITEC((uint32_t *) buffer++) != 0) {
             return -1;
         }
     }
 #else
-    uint8_t * str = alloca(count + 1);
-    memcpy(str, buf, count);
-    str[count] = 0;
+    uint8_t * str;
+    // Yes, I like to live dangerously.
+    if (buffer[count]) {
+        str = alloca(count + 1);
+        memcpy(str, buf, count);
+        str[count] = 0;
+    } else {
+        str = (uint8_t *) (uint32_t *) buf;
+    }
     Semihost_SYS_WRITE0((uint32_t *) str);
 #endif
 
