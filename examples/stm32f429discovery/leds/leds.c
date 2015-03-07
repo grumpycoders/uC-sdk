34aa7f05b292e60325426ccf4ec99ba57399f156
Thomas Pietrzak <thomas.pietrzak@gmail.com>
2015-03-07 18:36:04 +0100

    LED example for stm32f429discovery

---
+++ examples/stm32f429discovery/leds/leds.c
@@ -0,0 +1,15 @@
+#include <gpio.h>
+
+int main() {
+    //Initialize the pin_t structure with the pin port and number
+    //On this board there is a LED on PG13
+    pin_t pin = MAKE_PIN(GPIO_PORT_G, 13);
+
+    //configure the pin for output. 
+    gpio_config(pin, pin_dir_write, pull_down);
+
+    //set the pin to HIGH
+    gpio_set(pin, 1);
+
+    return 0;
+}
