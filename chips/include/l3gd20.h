435ee9e5e809c91c6e4fe7bd2e98cf0846fda06c
Thomas Pietrzak <thomas.pietrzak@gmail.com>
2015-03-11 00:26:33 +0100

    update l3gd20

--- chips/include/l3gd20.h
+++ chips/include/l3gd20.h
@@ -7,30 +7,49 @@
 
 // ought to be generic, cm3-specific for now
 typedef enum {
-    l3gd20_i2c,
-    l3gd20_spi,
+    L3GD20_I2C,
+    L3GD20_SPI,
 } l3gd20_communication_t;
 
+typedef enum {
+    L3GD20_250DPS  = 0x00,
+    L3GD20_500DPS  = 0x01,
+    L3GD20_1000DPS = 0x02,//check if this was a typo or if it is 2000
+    L3GD20_2000DPS = 0x03,
+}l3gd20_scale_t;
+
+typedef enum {
+    L3GD20_95HZ    = 0x00,
+    L3GD20_190HZ   = 0x01,
+    L3GD20_380HZ   = 0x02,
+    L3GD20_760HZ   = 0x03,
+}l3gd20_datarate_t;
+
+typedef enum {
+    L3GD20_BW1     = 0x00,
+    L3GD20_BW2     = 0x01,
+    L3GD20_BW3     = 0x02,
+    L3GD20_BW4     = 0x03,
+}l3gd20_bandwidth_t;
+
 typedef struct {
-    l3gd20_communication_t comm;
-    ssp_port_t ssp;
-//    i2c_port_t i2c;
+    // these are to be filled by caller
     pin_t cs;
-    float sensitivity;
+    // private data
+    l3gd20_communication_t comm;
+    ssp_t ssp;
+//    i2c_t i2c;
+    uint8_t odr:2, bw:2, scale:2, power:1;
 } l3gd20_t;
 
 
-l3gd20_t *l3gd20_init_ssp(ssp_port_t port, pin_t cs);
-l3gd20_t *l3gd20_init_i2c(ssp_port_t port, pin_t cs);
+int l3gd20_init_ssp(l3gd20_t *l3gd20, ssp_port_t ssp_port);
+//int l3gd20_init_i2c(l3gd20_t *l3gd20, i2c_port_t port);
 
-void l3gd20_setup_filter(l3gd20_t *sensor, float cutoff);
+void l3gd20_power(l3gd20_t *l3gd20, int power);
+void l3gd20_scale(l3gd20_t *l3gd20, l3gd20_scale_t scale);
+void l3gd20_frequency(l3gd20_t *l3gd20, l3gd20_datarate_t odr, l3gd20_bandwidth_t bw);
+void l3gd20_filter(l3gd20_t *l3gd20, float cutoff);
+void l3gd20_read(l3gd20_t *l3gd20, float axis[3]);
 
-int l3gd20_power_on(l3gd20_t *sensor, int power);
-
-uint8_t l3gd20_who_am_i(l3gd20_t *sensor);
-
-void l3gd20_read(l3gd20_t *sensor, float axis[3]);
-
-void l3gd20_set_sensitivity(l3gd20_t *sensor);
-void l3gd20_set_datarate(l3gd20_t *sensor);
 
--- chips/src/l3gd20.c
+++ chips/src/l3gd20.c
@@ -27,107 +27,170 @@ enum{
     L3GD20_INT1_THS_YL_REG  = 0x35,
     L3GD20_INT1_THS_ZH_REG  = 0x36,
     L3GD20_INT1_THS_ZL_REG  = 0x37,
-    L3GD20_INT1_DURATION_REG = 0x38
+    L3GD20_INT1_DURATION_REG = 0x38,
 } l3gd20_registers_t;
 
-#define READ_BYTE 0x80
-#define MULTIBYTE_BYTE 0x40
+enum{
+    READ_BYTE = 0x80,
+    MULTIBYTE_BYTE = 0x40,
+};
 
-void spi_get_register(l3gd20_t *sensor, uint8_t address, uint8_t *buffer, uint8_t nb){
+
+void spi_read_registers(ssp_t ssp, uint8_t address, uint8_t *buffer, uint8_t nb) {
     //set the read bit
     address |= READ_BYTE;
 
     //set multiple byte bit if necessary
     if (nb > 1)
         address |= MULTIBYTE_BYTE;
-    gpio_set(sensor->cs, 0);
     //send the request
-    ssp_write(sensor->ssp, address);
+    ssp_write(ssp, address);
 
     //get the value
     while(nb--)
-        *buffer++ = ssp_read(sensor->ssp);
-    gpio_set(sensor->cs, 1);
+        *buffer++ = ssp_read(ssp);
 }
 
-void spi_set_register(l3gd20_t *sensor, uint8_t address, uint8_t *buffer, uint8_t nb){
+void spi_write_register(ssp_t ssp, uint8_t address, uint8_t value) {
     //reset the read bit
     address &= (0xff ^ READ_BYTE);
 
-    //set multiple byte bit if necessary
-    if (nb > 1)
-        address |= MULTIBYTE_BYTE;
-    gpio_set(sensor->cs, 0);
     //send the request
-    ssp_write(sensor->ssp, address);
+    ssp_write(ssp, address);
 
     //send the value
-    while(nb--)
-        ssp_write(sensor->ssp, *buffer++);
-    gpio_set(sensor->cs, 1);
+    ssp_write(ssp, value);
 }
 
+int l3gd20_init_ssp(l3gd20_t *l3gd20, ssp_port_t ssp_port) {
+    pin_t cs = l3gd20->cs;
+    ssp_t ssp = get_ssp(ssp_port);
+    l3gd20->ssp = ssp;
+    l3gd20->comm = L3GD20_SPI;
 
-l3gd20_t *l3gd20_init_ssp(ssp_port_t port, pin_t cs){
-    l3gd20_t *handle = (l3gd20_t *)malloc(sizeof(l3gd20_t));
+    gpio_set(cs, 1);
 
-    handle-> comm = l3gd20_spi;
-    handle->ssp = port;
-    handle->cs = cs;
+    ssp_config(ssp_port, 8 * 1000 * 1000);
 
     uint8_t b;
-    b = 0x08 | 0x00 | 0x01 | 0x02 | 0x10;
-    spi_set_register(handle, 0x20, &b, 1);
-    b = 0x00 | 0x04;
-    spi_set_register(handle, 0x21, &b, 1);
-    b = 0x00 | 0x00 | 0x00;
-    spi_set_register(handle, 0x23, &b, 1);
-    b = 0x40;
-    spi_set_register(handle, 0x2e, &b, 1);
 
-    handle->sensitivity = 8.75e-3;
+    // Let's read WHO_AM_I (0x0f) - should be 0xd4 for L3GD20.
+    gpio_set(cs, 0);
+    spi_read_registers(ssp, 0x0f, &b, 1);
+    gpio_set(cs, 1);
 
-    return handle;
+    if (b != 0xd4) return 0;
+
+    l3gd20_frequency(l3gd20, L3GD20_190HZ, L3GD20_BW1);
+    l3gd20_power(l3gd20, 1);
+    l3gd20_scale(l3gd20, L3GD20_250DPS);
+
+    return 1;
 }
 
-l3gd20_t *l3gd20_init_i2c(ssp_port_t port, pin_t cs){
+/*
+int l3gd20_init_i2c(l3gd20_t *l3gd20, i2c_port_t port){
     return NULL;
+}
+*/
+
+void l3gd20_power(l3gd20_t *l3gd20, int power) {
+    pin_t cs = l3gd20->cs;
+    ssp_t ssp = l3gd20->ssp;
+
+    gpio_set(cs, 1);
+
+    uint8_t b;
+
+    // Let's write to CTRL_REG1
+    // 0x67 = 00000111
+    // bit0: Enable X
+    // bit1: Enable Y
+    // bit2: Enable Z
+    // bit3: Power Down
+    // bit4-bit5: BW = bandwidth selection
+    // bit6-bit7: ODR = sample frequency
+    b = l3gd20->odr << 6 | l3gd20->bw << 4 | 0x07; // enable X, Y, Z
+    if (power) b |= 0x08;
+    l3gd20->power = power ? 1 : 0;
+
+    gpio_set(cs, 0);
+    spi_write_register(ssp, L3GD20_CTRL_REG1, b);
+    gpio_set(cs, 1);
+}
+
+void l3gd20_scale(l3gd20_t *l3gd20, l3gd20_scale_t scale) {
+    pin_t cs = l3gd20->cs;
+    ssp_t ssp = l3gd20->ssp;
+
+    gpio_set(cs, 1);
+
+    uint8_t b;
+
+    // Let's write to CTRL_REG4
+    // Basically, setting defaults, just in case.
+    // bit0: SPI mode: 4 wires
+    // bit1-2: must be 0
+    // bit3: nothing
+    // bit4-bit5 : Full scale selection
+    // bit6: little endian selection
+    // bit7: block data update: continuous
+    b = scale << 4;
+    l3gd20->scale = scale;
+    gpio_set(cs, 0);
+    spi_write_register(ssp, L3GD20_CTRL_REG4, b);
+    gpio_set(cs, 1);
+}
+
+void l3gd20_frequency(l3gd20_t *l3gd20, l3gd20_datarate_t odr, l3gd20_bandwidth_t bw) {
+    l3gd20->odr = odr;
+    l3gd20->bw = bw;
+    l3gd20_power(l3gd20, l3gd20->power);
+}
+
+void l3gd20_read(l3gd20_t *l3gd20, float axis[3]) {
+    pin_t cs = l3gd20->cs;
+    ssp_t ssp = l3gd20->ssp;
+
+    gpio_set(cs, 1);
+
+    uint8_t registers[6];
+    gpio_set(cs, 0);
+    spi_read_registers(ssp, L3GD20_OUT_X_L, registers, 6);
+    gpio_set(cs, 1);
+
+    float sensitivity = 0.0f;
+
+    // Reading scaling register from CTRL_REG5 (0x24)
+    // That's bits 3 to 5.
+    switch (l3gd20->scale) {
+    case L3GD20_250DPS:
+        sensitivity = 8.75f;
+        break;
+    case L3GD20_500DPS:
+        sensitivity = 17.5f;
+        break;
+    case L3GD20_1000DPS:
+        sensitivity = 35.0f;
+        break;
+    case L3GD20_2000DPS:
+        sensitivity = 70.0f;
+        break;
+    }
+
+    int i;
+    // Now let's read X, Y, and Z.
+    // That's registers 0x28 to 0x2d.
+    // X is in 0x28 - 0x29.
+    // Y is in 0x2a - 0x2b.
+    // Z is in 0x2c - 0x2d.
+    int16_t * axis_data = (int16_t *) registers;
+    for (i = 0; i < 3; i++) {
+        axis[i] = sensitivity * axis_data[i];
+    }
 }
 
 void l3gd20_setup_filter(l3gd20_t *sensor, float cutoff){
 }
 
-void l3gd20_power(l3gd20_t *sensor, int power){
-  uint8_t b;
-  spi_get_register(sensor, L3GD20_CTRL_REG1, &b, 1);
-  b &= 0xF7;
-
-  if (power)
-      b |= (uint8_t) 0x08;
-  spi_set_register(sensor, L3GD20_CTRL_REG1, &b, 1);
-}
-
-uint8_t l3gd20_who_am_i(l3gd20_t *sensor){
-    uint8_t b;
-    spi_get_register(sensor, L3GD20_WHO_AM_I, &b, 1);
-    return b;
-}
-void l3gd20_read(l3gd20_t *sensor, float axis[3]){
-    uint8_t b[6];
-    int16_t r;
-    spi_get_register(sensor, L3GD20_OUT_X_L, b, 6);
-    uint8_t i;
-    for (i = 0 ; i < 3 ; i++){
-        r = (int16_t)(((uint16_t) b[2*i+1]) << 8 | b[2*i]);
-        axis[i] = r * sensor->sensitivity;
-    }
-}
-
-void l3gd20_set_sensitivity(l3gd20_t *sensor){
-
-}
-
-void l3gd20_set_datarate(l3gd20_t *sensor){
-
-}
 
