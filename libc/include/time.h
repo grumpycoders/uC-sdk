5fabf2ee4c7e0a1ab5a1331d54a8141f7eb3e2b8
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2015-04-12 09:31:37 +0200

    Adding basic time.h

---
+++ libc/include/time.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef uint64_t time_t;
+
+struct tm {
+    int tm_sec;
+    int tm_min;
+    int tm_hour;
+    int tm_mday;
+    int tm_mon;
+    int tm_year;
+    int tm_wday;
+    int tm_yday;
+    int tm_isdst;
+};
+
+struct timespec {
+    time_t tv_sec;
+    long tv_nsec;
+};
