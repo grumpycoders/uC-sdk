23cc220f1c7a9c6b3ed6e7f28a8e527f53cf9660
Thomas Pietrzak <thomas.pietrzak@gmail.com>
2017-10-03 20:24:27 +0200

    fixed wrong entry point

--- target-rules.mk
+++ target-rules.mk
@@ -41,7 +41,7 @@ endif
 
 $(TARGET_ELF): $(TARGET_OBJS) $(LIBDEPS) $(LDSCRIPT)
 	$(E) "[TL]     Linking $@"
-	$(Q)$(TARGET_LD) -Wl,--gc-sections -Wl,-Map=$(TARGET_MAP) -o $@ $(TARGET_OBJS) $(TARGET_LDFLAGS) -g -T$(LDSCRIPT) -Wl,--entry=_ucsdk_start $(LIBS)
+	$(Q)$(TARGET_LD) -Wl,--gc-sections -Wl,-Map=$(TARGET_MAP) -o $@ $(TARGET_OBJS) $(TARGET_LDFLAGS) -g -T$(LDSCRIPT) $(LIBS)
 
 $(TARGET_MAP): $(TARGET_ELF)
 
