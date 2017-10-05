116d2464d3f9d30e5e68f54a212e949839e7ecda
Thomas Pietrzak <thomas.pietrzak@gmail.com>
2017-10-05 22:58:12 +0200

    removed outdated fix for HSE selection

--- arch/arm/stm32f4xx/stm32f4discovery/BoardInit.c
+++ arch/arm/stm32f4xx/stm32f4discovery/BoardInit.c
@@ -1,10 +1,6 @@
 #include "stm32f4xx_rcc.h"
 
 void BoardEarlyInit() {
-	/* Select HSE as system clock source */
-    RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_SW));
-    RCC->CFGR |= (uint32_t)RCC_CFGR_SW_HSE;
-    SystemCoreClockUpdate();
 }
 
 void BoardInit() {
