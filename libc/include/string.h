e362aaab2ebd918ab6fa2cdee923d7d74a65b72e
Nicolas 'Pixel' Noble <nicolas@nobis-crew.org>
2020-07-12 22:39:28 -0700

    De-inlining some memory functions for freestanding gcc.

--- libc/include/string.h
+++ libc/include/string.h
@@ -4,60 +4,10 @@
 #include <stddef.h>
 #include <malloc.h>
 
-static __inline__ void * memcpy(void * _s1, const void * _s2, size_t n) {
-    uint8_t * s1 = (uint8_t *) _s1;
-    const uint8_t * s2 = (uint8_t *) _s2;
-    size_t i;
-    
-    for (i = 0; i < n; i++)
-        *s1++ = *s2++;
-    
-    return _s1;
-}
-
-static __inline__ void * memmove(void * _s1, const void * _s2, size_t n) {
-    uint8_t * s1 = (uint8_t *) _s1;
-    const uint8_t * s2 = (uint8_t *) _s2;
-    size_t i;
-    
-    if (s1 < s2) {
-        for (i = 0; i < n; i++)
-            *s1++ = *s2++;
-    } else if (s1 > s2) {
-        s1 += n;
-        s2 += n;
-        for (i = 0; i < n; i++)
-            *--s1 = *--s2;
-    }
-    
-    return _s1;
-}
-
-static __inline__ int memcmp(const void * _s1, const void * _s2, size_t n) {
-    uint8_t * s1 = (uint8_t *) _s1;
-    const uint8_t * s2 = (uint8_t *) _s2;
-    size_t i;
-    
-    for (i = 0; i < n; i++, s1++, s2++) {
-        if (*s1 < *s2) {
-            return -1;
-        } else if (*s1 > *s2) {
-            return 1;
-        }
-    }
-    
-    return 0;
-}
-
-static __inline__ void * memset(void * _s, int c, size_t n) {
-    uint8_t * s = (uint8_t *) _s;
-    size_t i;
-    
-    for (i = 0; i < n; i++)
-        *s++ = (uint8_t) c;
-    
-    return _s;
-}
+void * memcpy(void * _s1, const void * _s2, size_t n);
+void * memmove(void * _s1, const void * _s2, size_t n);
+int memcmp(const void * _s1, const void * _s2, size_t n);
+void * memset(void * _s, int c, size_t n);
 
 static __inline__ const void * memchr(const void * _s, int c, size_t n) {
     const uint8_t * s = (uint8_t *) _s;
